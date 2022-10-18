21d0cc4c588cbbfa5e326163e9f46c5fabdc4de9
Michael Fulton <fulto081@umn.edu>
2022-10-18 12:46:49 -0500

    Implemented Weighted Average filter.

--- dlclive/processor/posefilter.py
+++ dlclive/processor/posefilter.py
@@ -1,7 +1,7 @@
 """"
 IRVLab GPLV3
 
-A low pass filter for pose estimators.
+A set of filters for pose estimators.
 
 """
 
@@ -61,105 +61,117 @@ class OneEuroFilter:
             
             return self.x_prev
 
+class MovingAverageFilter:
+    def __init__(self, time, pose, pcutoff=0.5, maximum_distance=50, memory_length=5):
+        self.pcutoff = pcutoff
+        self.maximum_distance = maximum_distance
+        self.memory = np.zeros((memory_length, 4))
+
+        # Set initial values of memory, if the estimate is good enough.
+        if pose[2] > self.pcutoff:
+            self.memory[-1] = [time, pose[0], pose[1], pose[2]]
+
+    def __call__(self, time, pose):
+        x,y,conf = pose
+
+        # If we believe the estimate
+        if self.pose_acceptable(pose):
+            # First, update the memory. We're storing, but not using time. In the future we could use time distance as another 
+            self.memory[:-1] = self.memory[1:]
+            self.memory[-1] = [time, x, y, conf]
+
+        # If we don't believe in the estimate
+        else:
+            # Remove the first element, as long as the list isn't empty. 
+            # We do this to keep the estimate recent.
+            self.memory[:-1] = self.memory[1:]
+            self.memory[-1] = [0, 0 ,0, 0]
+        
+        # If we have more than nothing in the memory, compute the mean.
+        if np.any(self.memory):
+            # print("Memory at this point: {}".format(self.memory))
+            # print("Memory used for mean: {}".format(self.memory[~np.all(self.memory == 0, axis=1), 1:]))
+
+            # Then, compute the average of the memory (only the non-zero rows)
+            relevent_memory = self.memory[~np.all(self.memory == 0, axis=1), 1:]
+            # We calculate weights by inverse time distance, scaled by confidence.
+            weights = abs((relevent_memory[:,0] - time)/max(relevent_memory[:,0]) - 1) * relevent_memory[:,3]
+            x_mean, y_mean, conf_mean = np.mean(relevent_memory, axis=0, weights= weights)
+
+            # print("Calculated mean: {}".format([x_mean, y_mean, conf_mean]))
+            # Return the average of the memory.
+            return [x_mean, y_mean, conf_mean]
+
+        # If we don't have enough in the memory, just return it as is.
+        else:
+            return [x, y, conf]
+
+    def pose_acceptable(self, pose):
+        x,y,conf = pose
+
+        conf_term = (conf > self.pcutoff)
+
+        x_dist = abs(self.memory[-1, 1] - x)
+        x_term = x_dist < self.maximum_distance
+
+        y_dist = abs(self.memory[-1, 2] - y)
+        y_term = y_dist < self.maximum_distance
+
+        return conf_term and x_term and y_term
+
+
+class KalmanFilter(object):
+    def __init__(self):
+        pass
+
 class PoseFilter(Processor):
-    def __init__(self, minimum_cutoff=0.005, beta=0.7, confidence_thresh=0.8, memory_length=5, max_center_dist=500, max_self_dist=50, **kwargs,):
+    def __init__(self,  **kwargs,):
         super().__init__(**kwargs)
-        self.min_cutoff = minimum_cutoff
-        self.beta = beta
-        self.point_filters = list()
+        self.mode = kwargs['filter_type']
 
-        self.confidence_thresh= confidence_thresh
+        if self.mode == "low_pass":
+            self.min_cutoff = kwargs['minimum_cutoff']
+            self.beta = kwargs['beta']
+            self.point_filters = list()
 
-        self.memory_length = memory_length
-        self.past_poses = [None] * memory_length
-        self.past_pose_centers = [None] * memory_length
-        self.past_pose_times = [None] * memory_length
-        self.max_center_dist = max_center_dist
-        self.max_self_dist = max_self_dist
-    
-    def reset(self):
-        self.__init__()
+        elif self.mode == "moving_average":
+            self.pcutoff = kwargs['pcutoff']
+            self.memory_length = kwargs['memory_length']
+            self.maximum_distance = kwargs['maximum_distance']
+            self.point_filters = list()
 
-    def calculate_pose_center(self, pose):
-        x_sum = np.sum(pose[:,0])
-        y_sum = np.sum(pose[:,1])
-        centroid = (int(x_sum/len(pose)), int(y_sum/len(pose)))
-        return centroid
-
-    def check_pose_dist(self, c, bp):
-        distance = math.sqrt((c[0] - bp[0])**2 + (c[1] - bp[1])**2 )
-        return (distance < self.max_center_dist)
-        
-    def check_prev_dist(self, k, bp, current_time):
-        pose = self.past_poses[-1]
-        if pose is None:
-            return True
-
-        # last_time = self.past_pose_times[-1]
-        distance = math.sqrt((pose[k][0] - bp[0])**2 + (pose[k][1] - bp[1])**2 )
-        
-        return (distance < self.max_self_dist)
-
-    def average_past_point(self, k):
-        x_avg, y_avg, w_sum, n = [0,0,0,0]
-        for pose in self.past_poses:
-            if pose is not None: #Check for None
-                x_avg += pose[k][0] * (pose[k][2] * 100)
-                y_avg += pose[k][1] * (pose[k][2] * 100)
-                w_sum += pose[k][2] * 100
-                n += 1
-        
-        if n == self.memory_length:
-            x_avg = x_avg/w_sum
-            y_avg = y_avg/w_sum
-            conf_avg = (w_sum/n)/100
-
-            return (int(x_avg),int(y_avg), conf_avg)
+        elif self.mode == "kalman_filter":
+            pass
 
         else:
-            return None
+            raise NotImplementedError("No such filtering algorithm has been implemented for the PoseFilter.")
+    
+    def reset(self):
+        if self.mode == "low_pass":
+            self.__init__(filter_type=self.mode, minimum_cutoff=self.min_cutoff, beta=self.beta)
+        elif self.mode == "moving_average":
+            self.__init__(filter_type=self.mode, pcutoff=self.pcutoff, maximum_distance=self.maximum_distance, memory_length=self.memory_length)
 
     def process(self, pose, **kwargs):
         time = kwargs['frame_time']
 
-        # First, run the points through a point-by-point filtering process to smooth them out a little bit.
-        for k, bp in enumerate(pose):
-            try:
-                x_filter, y_filter = self.point_filters[k]
-                pose[k][0] = x_filter(time, bp[0])
-                pose[k][1] = y_filter(time, bp[1])
-            # We hit this if there's no initialized filter.
-            except IndexError:
-                self.point_filters.append((OneEuroFilter(time, bp[0], self.min_cutoff, self.beta), OneEuroFilter(time, bp[1], self.min_cutoff, self.beta)))
+        if self.mode == "one_euro":
+            for k, bp in enumerate(pose):
+                try:
+                    x_filter, y_filter = self.point_filters[k]
+                    pose[k][0] = x_filter(time, bp[0])
+                    pose[k][1] = y_filter(time, bp[1])
+                # We hit this if there's no initialized filter.
+                except IndexError:
+                    self.point_filters.append((OneEuroFilter(time, bp[0], self.min_cutoff, self.beta), OneEuroFilter(time, bp[1], self.min_cutoff, self.beta)))
+        elif self.mode == "moving_average":
+            for k, bp in enumerate(pose):
+                try:
+                    pfilter = self.point_filters[k]
+                    pose[k][0], pose[k][1], pose[k][2]  = pfilter(time, bp)
+                # We hit this if there's no initialized filter.
+                except IndexError:
+                    self.point_filters.append(MovingAverageFilter(time, bp, pcutoff=self.pcutoff, maximum_distance=self.maximum_distance, memory_length=self.maximum_distance))
 
         
-        # Next, let's make sure that the points are all within a reasonable range from the pose center (which we'll also calculate here.)
-        # If a point is not a reasonable distance from the pose center, we'll generate a point based on our memory of the last N points.
-        pose_center = self.calculate_pose_center(pose)
-        for k, bp in enumerate(pose):
-            if self.check_pose_dist(pose_center,bp):
-                pass
-            elif self.check_prev_dist(k, bp, time):
-                pass
-            elif bp[2] >= self.confidence_thresh:
-                pass
-            else:
-                average_of_past = self.average_past_point(k)
-                if average_of_past is not None:
-                    pose[k][0] = average_of_past[0]
-                    pose[k][1] = average_of_past[1]
-                    pose[k][2] = average_of_past[2]
-                else:
-                    pass # There's nothing we can do if there's not enough past data.
-
-        #Lastly, update memory. 
-        self.past_poses.pop(0)
-        self.past_poses.append(pose)
-
-        self.past_pose_times.pop(0)
-        self.past_pose_times.append(time)
-
-        self.past_pose_centers.pop(0)
-        self.past_pose_centers.append(pose_center)
-
         return pose
